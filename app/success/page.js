5a30284d0b2b8a7c90adcf869d4e5b2bdcfcffbe
arpita330 <arpitanaha721@gmail.com>
2026-02-20 03:06:31 +0530

    Create page.js

---
+++ app/success/page.js
@@ -0,0 +1,9 @@
+export default function Success() {
+  return (
+    <div className="card">
+      <h2>✅ Success</h2>
+      <p>Your demo gift has been claimed.</p>
+      <div className="success">OTP Verified Successfully</div>
+    </div>
+  );
+    }
