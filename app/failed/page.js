52df7e3a125edd049c4663f9a416d8682a429345
arpita330 <arpitanaha721@gmail.com>
2026-02-20 03:07:06 +0530

    Create page.js

---
+++ app/failed/page.js
@@ -0,0 +1,9 @@
+export default function Failed() {
+  return (
+    <div className="card">
+      <h2>❌ Failed</h2>
+      <p>Invalid OTP. Please try again.</p>
+      <div className="error">Verification Failed</div>
+    </div>
+  );
+}
